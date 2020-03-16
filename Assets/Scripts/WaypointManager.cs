db393b742f4d3ea1399c629a3d11de5873c830da
Tan Jun Ming <jtan@csumb.edu>
2020-03-15 20:47:14 -0700

    Add waypoints

---
+++ Assets/Scripts/WaypointManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointManager : MonoBehaviour
+{
+    public Transform waypoint_list;
+
+    private List<Transform> waypoints = new List<Transform>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        int i = 0;
+        while (true)
+        {
+            Transform new_waypoint = waypoint_list.Find(i.ToString());
+            if (new_waypoint == null)
+            {
+                break;
+            }
+            waypoints.Add(new_waypoint);
+            i++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
